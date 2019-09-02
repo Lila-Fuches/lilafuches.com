3c5d72ddf2d873b8a9b33c4e5bac29f57675f0df
Steve McDougall <juststeveking@gmail.com>
2019-09-02 16:32:22 +0100

    And voila a website is born

---
+++ gridsome.config.js
@@ -0,0 +1,87 @@
+// This is where project configuration and plugin options are located.
+// Learn more: https://gridsome.org/docs/config
+
+// Changes here require a server restart.
+// To restart press CTRL + C in terminal and run `gridsome develop`
+
+const tailwind = require('tailwindcss')
+const purgecss = require('@fullhuman/postcss-purgecss')
+
+const postcssPlugins = [
+  tailwind(),
+]
+
+if (process.env.NODE_ENV === 'production') postcssPlugins.push(purgecss())
+
+module.exports = {
+  siteName: 'Lila Fuches',
+  siteDescription: 'Software for any business, we help companies turn great ideas into awesome products',
+  siteUrl: 'https://www.lilafuches.com',
+  icon: {
+    favicon: './src/favicon.ico',
+    touchicon: './src/touchicon.ico'
+  },
+  plugins: [
+    {
+      use: '@gridsome/source-filesystem',
+      options: {
+        path: 'blog/**/*.md',
+        typeName: 'Post',
+        refs: {
+          tags: {
+            typeName: 'Tag',
+            route: 'tag/:id',
+            create: true
+          }
+        },
+        remark: {
+          plugins: [
+            [ 'gridsome-plugin-remark-shiki', { theme: 'Material-Theme-Palenight', skipInline: true } ]
+          ]
+        }
+      }
+    },
+    {
+      use: 'gridsome-plugin-rss',
+      options: {
+        contentTypeName: 'Post',
+        feedOptions: {
+          title: 'Lila Fuches Blog',
+          feed_url: 'https://www.lilafuches.com/rss.xml',
+          site_url: 'https://www.lilafuches.com/'
+        },
+        feedItemOptions: node => ({
+          title: node.title,
+          description: node.summary,
+          url: 'https://www.lilafuches.com' + node.path,
+          author: 'Lila Fuches',
+          date: node.date
+        }),
+        output: {
+          dir: './static',
+          name: 'rss.xml'
+        }
+      }
+    },
+    {
+      use: '@gridsome/plugin-sitemap',
+      options: {
+        cacheTime: 600000, // default
+      }
+    },
+  ],
+  transformers: {
+    remark: {
+      externalLinksTarget: '_blank',
+      externalLinksRel: ['nofollow', 'noopener', 'noreferrer'],
+      anchorClassName: 'icon icon-link',
+    }
+  },
+  css: {
+    loaderOptions: {
+      postcss: {
+        plugins: postcssPlugins,
+      },
+    },
+  },
+}
