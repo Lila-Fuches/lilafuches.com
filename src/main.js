cc8a9c837590351d0f09dcfe8e3fbfc71717c60c
Steve McDougall <juststeveking@gmail.com>
2019-09-03 12:55:08 +0100

    Adding .env

--- src/main.js
+++ src/main.js
@@ -37,6 +37,20 @@ export default function (Vue, { router, head, isClient }) {
     name: 'twitter:description',
     content: `Software for any business, we help companies turn great ideas into awesome products`,
   })
+
+  router.beforeEach((to, _from, next) => {
+    head.meta.push({
+      key: 'og:url',
+      name: 'og:url',
+      content: process.env.GRIDSOME_BASE_PATH + to.path,
+    })
+    head.meta.push({
+      key: 'og:image',
+      name: 'og:image',
+      content: process.env.GRIDSOME_BASE_PATH + 'static/logo-head.png',
+    })
+    next()
+  })
 }
 
 
